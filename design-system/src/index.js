8205fb1fb492e2a67f9a5dd7e2636eb0864fc40e
Bridge Tools <robot@bridgetools.io>
2021-05-05 15:13:33 +0300

    export spacing and do not export avatar

--- design-system/src/index.js
+++ design-system/src/index.js
@@ -5,10 +5,9 @@
  */
 
 export { default as GlobalStyle } from "./global.js";
-export { colors, typography } from "./tokens";
+export { colors, typography, spacing } from "./tokens";
 
 
-export { default as Avatar } from "./components/Avatar";
 export { default as Button } from "./components/Button";
 export { default as Carousel } from "./components/Carousel";
 export { default as Icon } from "./components/Icon";
